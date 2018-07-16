30909ee3a9d39ad2b052221d3457d640e03bae31
刘泽华 <Zehua.Liu@britishcouncil.org.cn>
2018-07-16 15:54:24 +0800

    iview

--- APP.UI.Admin/ClientApp/src/app/main/main.ts
+++ APP.UI.Admin/ClientApp/src/app/main/main.ts
@@ -1,5 +1,7 @@
 import { Component, Vue, Prop } from 'vue-property-decorator';
 import SideMenu from './components/side-menu/side-menu.vue';
+import minLogo from '@/assets/images/logo-min.jpg';
+import maxLogo from '@/assets/images/logo.jpg';
 
 @Component({
     components: {
@@ -7,4 +9,8 @@ import SideMenu from './components/side-menu/side-menu.vue';
     },
 })
 export default class MainComponent extends Vue {
+    @Prop() private collapsed: boolean = false;
+    @Prop() private minLogo: string = minLogo;
+    @Prop() private maxLogo: string = maxLogo;
+    @Prop() private isFullscreen: boolean = false;
 }
--- APP.UI.Admin/ClientApp/src/app/main/main.vue
+++ APP.UI.Admin/ClientApp/src/app/main/main.vue
@@ -1,6 +1,36 @@
 <template>
     <Layout style="height: 100%" class="main">
-        
+        <Sider hide-trigger collapsible :width="210" :collapsed-width="64" v-model="collapsed">
+            <Header class="header-con">
+                <header-bar :collapsed="collapsed" @on-coll-change="handleCollapsedChange">
+                    <user :user-avator="userAvator" />
+                    <language @on-lang-change="setLocal" style="margin-right: 10px;" :lang="local" />
+                    <fullscreen v-model="isFullscreen" style="margin-right: 10px;" />
+                </header-bar>
+            </Header>
+            <side-menu accordion :active-name="$route.name" :collapsed="collapsed" @on-select="turnToPage" :menu-list="menuList">
+                <!-- 需要放在菜单上面的内容，如Logo，写在side-menu标签内部，如下 -->
+                <div class="logo-con">
+                    <img v-show="!collapsed" :src="maxLogo" key="max-logo" />
+                    <img v-show="collapsed" :src="minLogo" key="min-logo" />
+                </div>
+            </side-menu>
+        </Sider>
+        <Layout>
+            
+            <Content>
+                <Layout>
+                    <div class="tag-nav-wrapper">
+                        <tags-nav :value="$route" @input="handleClick" :list="tagNavList" @on-close="handleCloseTag" />
+                    </div>
+                    <Content class="content-wrapper">
+                        <keep-alive :include="cacheList">
+                            <router-view />
+                        </keep-alive>
+                    </Content>
+                </Layout>
+            </Content>
+        </Layout>
     </Layout>
 </template>
 
