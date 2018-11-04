1a32952187ea9ac9e279ee041148e389ea3ce4be
LZH <mail@521.org.cn>
2018-11-05 07:44:39 +0800

    add vuex

--- APP.UI.Admin/ClientApp/src/store/auth.ts
+++ APP.UI.Admin/ClientApp/src/store/auth.ts
@@ -1,18 +1,8 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
 
-Vue.use(Vuex);
-
-export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
+export const auth = {
   actions: {
-
-  },
-  modules: {
+    signIn() {
+      debugger;
+    }
   }
-});
+};
--- APP.UI.Admin/ClientApp/src/store/index.ts
+++ APP.UI.Admin/ClientApp/src/store/index.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import {auth} from './auth';
 
 Vue.use(Vuex);
 
@@ -14,5 +15,6 @@ export default new Vuex.Store({
 
   },
   modules: {
+    auth
   }
 });
