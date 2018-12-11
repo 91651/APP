b3bd05895d4217b39c532d00092ef2e6e4320a39
刘泽华 <Zehua.Liu@britishcouncil.org.cn>
2018-12-11 14:22:11 +0800

    iview 兼容性调整

--- APP.UI.Admin/ClientApp/babel.config.js
+++ APP.UI.Admin/ClientApp/babel.config.js
@@ -2,7 +2,14 @@ module.exports = {
   presets: [
     [
       '@vue/app', {
-        useBuiltIns: 'entry'
+        debug: true,
+        polyfills: [
+          'es6.promise',
+          //iview兼容性铺垫
+          'es6.array.find-index',
+          'es7.array.includes',
+          'es6.string.includes'
+        ]
       }
     ]
   ]
--- APP.UI.Admin/ClientApp/src/main.ts
+++ APP.UI.Admin/ClientApp/src/main.ts
@@ -1,10 +1,9 @@
-import '@babel/polyfill';
+// 浏览器使用fetch
 import 'whatwg-fetch';
 
 import Vue from 'vue';
 import router from './router';
 import store from './store/index';
-
 import 'iview/dist/styles/iview.css';
 import './global.less';
 import iView from 'iview';
@@ -16,11 +15,9 @@ import App from './app/app.vue';
 Vue.use(iView);
 
 moment.locale('zh-cn');
-
 Vue.use(vueMoment, {
   moment
 });
-
 
 new Vue({
   router,
