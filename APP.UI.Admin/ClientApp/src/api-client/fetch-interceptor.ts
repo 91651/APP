95ce3c667e329b7598c4e28bd5636fadebe8ed96
刘泽华 <Zehua.Liu@britishcouncil.org.cn>
2019-01-03 17:01:58 +0800

    http拦截器

---
+++ APP.UI.Admin/ClientApp/src/api-client/fetch-interceptor.ts
@@ -0,0 +1,15 @@
+﻿
+let interceptors: any[] = [];
+window.fetch = ((fetch) => {
+    return (url: string, request: RequestInit) => {
+        debugger;
+        return interceptor(fetch(url, request));
+    };
+})(window.fetch);
+
+function interceptor(fetch: any) {
+    const reversedInterceptors = interceptors.reduce((array, interceptor) => [interceptor].concat(array), []);
+
+
+    return fetch;
+}
