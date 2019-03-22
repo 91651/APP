ad36b96121d21c0446f4df1ca379c5dd07f2ed7e
刘泽华 <Zehua.Liu@britishcouncil.org.cn>
2019-03-22 18:36:44 +0800

    ant

--- APP.UI.Admin/ClientApp/src/app/sign-in/sign-in.ts
+++ APP.UI.Admin/ClientApp/src/app/sign-in/sign-in.ts
@@ -1,36 +1,22 @@
 import { Component, Vue, Prop } from 'vue-property-decorator';
-import { State, Action, Getter } from 'vuex-class';
 import './sign-in.less';
-
+import { _Auth } from '@/api-client';
 @Component
 export default class SignInComponent extends Vue {
-  @Action private signIn!: (data: {}) => Promise<any>;
+  private signInForm: any;
 
-  private signInForm: any = {
-        userName: '',
-        password: '',
-        message:''
-      };
-      private rules: any = {
-        userName: [
-            { required: true, message: '账号不能为空', trigger: 'blur' }
-        ],
-        password: [
-            { required: true, message: '密码不能为空', trigger: 'blur' }
-        ]
-      };
-      private handleSubmit(): void {
-          const form: any = this.$refs.signInForm;
-          form.validate((valid: any) => {
-            if (valid) {
-              this.signIn({name: this.signInForm.userName, pwd: this.signInForm.password}).then(r => {
-                if (r.token) {
-                  this.$router.replace('/');
-                } else {
-                  this.signInForm.message = r.status;
-                }
-              });
-            }
-          });
+  private beforeCreate() {
+    this.signInForm = this.$form.createForm(this);
+  }
+
+  private handleSubmit(e: Event): void {
+    e.preventDefault();
+    this.signInForm.validateFields((err: Error, values: any) => {
+      if (!err) {
+        _Auth.signIn(values.userName, values.password).then((d) => {
+          debugger;
+        });
       }
+    });
+  }
 }
--- APP.UI.Admin/ClientApp/src/app/sign-in/sign-in.vue
+++ APP.UI.Admin/ClientApp/src/app/sign-in/sign-in.vue
@@ -1,39 +1,49 @@
 <template>
-<div class="user-layout-container">
-<div class="user-layout-content">
-    <div class="user-layout-top"><div class="user-layout-header"><span class="user-layout-title"><a-icon type="smile" /> 遇见时光 </span></div><div class="user-layout-desc">总是热衷于代码的折腾，追求简洁与优雅！</div></div>
-<div class="user-login-main">
-<a-form :layout="'horizontal'">
-<a-tabs defaultActiveKey="1">
-    <a-tab-pane tab="账户密码登录" key="1">
-        <a-form-item>
-        <a-input size="large" placeholder="用户名"><a-icon slot="prefix" type="user" class="prefixIcon" /></a-input>
-      </a-form-item>
-      <a-form-item>
-        <a-input size="large" placeholder="密码"><a-icon slot="prefix" type="lock" class="prefixIcon" /></a-input>
-      </a-form-item>
-    </a-tab-pane>
-  </a-tabs>
-  <a-form-item>
-      <a-checkbox
-        v-decorator="[
-          'remember',
-          {
-            valuePropName: 'checked',
-            initialValue: true,
-          }]">
-        自动登录
-      </a-checkbox>
-      <a-button type="primary" block html-type="submit" class="login-form-button">
-        登录
-      </a-button>
-    </a-form-item>
-</a-form>
-</div>
-
-</div>
-<a-layout-footer>刘泽华 @2019</a-layout-footer>
-</div>
+  <div class="user-layout-container">
+    <div class="user-layout-content">
+      <div class="user-layout-top">
+        <div class="user-layout-header">
+          <span class="user-layout-title">
+            <a-icon type="smile"/>遇见时光
+          </span>
+        </div>
+        <div class="user-layout-desc">总是热衷于代码的折腾，追求简洁与优雅！</div>
+      </div>
+      <div class="user-login-main">
+        <a-form :layout="'horizontal'" :form="signInForm" @submit="handleSubmit">
+          <a-tabs defaultActiveKey="1">
+            <a-tab-pane tab="账户密码登录" key="1">
+              <a-form-item>
+                <a-input
+                  size="large"
+                  placeholder="用户名"
+                  v-decorator="['userName',{ rules: [{ required: true, message: '请输入用户账户!' }] }]"
+                >
+                  <a-icon slot="prefix" type="user" class="prefixIcon"/>
+                </a-input>
+              </a-form-item>
+              <a-form-item>
+                <a-input
+                  size="large"
+                  placeholder="密码"
+                  type="password"
+                  v-decorator="['password', { rules: [{ required: true, message: '请输入密码!' }] }]"
+                >
+                  <a-icon slot="prefix" type="lock" class="prefixIcon"/>
+                </a-input>
+              </a-form-item>
+              <a-alert message="Error" type="error" showIcon/>
+            </a-tab-pane>
+          </a-tabs>
+          <a-form-item>
+            <a-checkbox>自动登录</a-checkbox>
+            <a-button type="primary" block html-type="submit" class="login-form-button">登录</a-button>
+          </a-form-item>
+        </a-form>
+      </div>
+    </div>
+    <a-layout-footer>刘泽华 @2019</a-layout-footer>
+  </div>
 </template>
 
 <script lang="ts" src="./sign-in.ts"></script>
