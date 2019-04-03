e5d84d61770bcad7d3d0948b1a20fc84b8da4c73
刘泽华 <Zehua.Liu@britishcouncil.org.cn>
2019-04-03 18:50:16 +0800

    123

--- APP.UI.Admin/ClientApp/src/app/components/Menu/SideMenu.vue
+++ APP.UI.Admin/ClientApp/src/app/components/Menu/SideMenu.vue
@@ -1,21 +1,41 @@
     
 <template>
-  <a-layout-sider
-    :class="['sider', isDesktop() ? null : 'shadow', theme, fixSiderbar ? 'ant-fixed-sidemenu' : null ]"
-    width="256px"
-    :collapsible="collapsible"
-    v-model="collapsed"
-    :trigger="null">
-    <logo />
-    <s-menu
-      :collapsed="collapsed"
-      :menu="menus"
-      :theme="theme"
-      :mode="mode"
-      @select="onSelect"
-      style="padding: 16px 0px;"></s-menu>
+  <a-layout-sider breakpoint="lg" collapsedWidth="0">
+    <a-menu
+      mode="inline"
+      :defaultSelectedKeys="['1']"
+      :defaultOpenKeys="['sub1']"
+      :style="{ height: '100%', borderRight: 0 }"
+    >
+      <a-sub-menu key="sub1">
+        <span slot="title">
+          <a-icon type="user"/>subnav 1
+        </span>
+        <a-menu-item key="1">option1</a-menu-item>
+        <a-menu-item key="2">option2</a-menu-item>
+        <a-menu-item key="3">option3</a-menu-item>
+        <a-menu-item key="4">option4</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub2">
+        <span slot="title">
+          <a-icon type="laptop"/>subnav 2
+        </span>
+        <a-menu-item key="5">option5</a-menu-item>
+        <a-menu-item key="6">option6</a-menu-item>
+        <a-menu-item key="7">option7</a-menu-item>
+        <a-menu-item key="8">option8</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub3">
+        <span slot="title">
+          <a-icon type="notification"/>subnav 3
+        </span>
+        <a-menu-item key="9">option9</a-menu-item>
+        <a-menu-item key="10">option10</a-menu-item>
+        <a-menu-item key="11">option11</a-menu-item>
+        <a-menu-item key="12">option12</a-menu-item>
+      </a-sub-menu>
+    </a-menu>
   </a-layout-sider>
-
 </template>
 
 <script lang="ts" src="./SideMenu.ts"></script>
--- APP.UI.Admin/ClientApp/src/app/main/main.vue
+++ APP.UI.Admin/ClientApp/src/app/main/main.vue
@@ -1,7 +1,7 @@
 <template>
   <a-layout id="components-layout-demo-top-side-2">
     <a-layout-header class="header">
-      <div class="logo" />
+      <div class="logo"/>
       <a-menu
         theme="dark"
         mode="horizontal"
@@ -14,23 +14,16 @@
       </a-menu>
     </a-layout-header>
     <a-layout>
-      <side-menu
-        mode="inline"
-        :menus="menus"
-        :theme="navTheme"
-        :collapsed="false"
-        :collapsible="true"
-        @menuSelect="menuSelect"
-      ></side-menu>
+      <side-menu/>
       <a-layout style="padding: 0 24px 24px">
         <a-breadcrumb style="margin: 16px 0">
           <a-breadcrumb-item>Home</a-breadcrumb-item>
           <a-breadcrumb-item>List</a-breadcrumb-item>
           <a-breadcrumb-item>App</a-breadcrumb-item>
         </a-breadcrumb>
-        <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
-          Content
-        </a-layout-content>
+        <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >Content</a-layout-content>
       </a-layout>
     </a-layout>
   </a-layout>
