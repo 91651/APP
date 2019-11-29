fd3e2cde2787617ea64e912e447cd7ad8b8c822b
LZH <mail@521.org.cn>
2019-11-29 17:04:56 +0800

    添加 swagger

--- APP.Business.WebApi/Startup.cs
+++ APP.Business.WebApi/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using NSwag.AspNetCore;
 
 namespace APP.Business.WebApi
 {
@@ -25,6 +20,7 @@ namespace APP.Business.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddOpenApiDocument();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -39,6 +35,15 @@ namespace APP.Business.WebApi
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseOpenApi(settings =>
+            {
+                settings.PostProcess = (document, request) =>
+                {
+                    document.Info.Title = Configuration["Project:Name"];
+                };
+            });
+            app.UseSwaggerUi3();
 
             app.UseEndpoints(endpoints =>
             {
