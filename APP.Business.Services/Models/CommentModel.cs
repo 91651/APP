f904a2b41755463f73dbeed98f3a4729eab3b1dc
LZH <mail@521.org.cn>
2020-06-01 23:21:33 +0800

    function dev

---
+++ APP.Business.Services/Models/CommentModel.cs
@@ -0,0 +1,9 @@
+﻿namespace APP.Business.Services.Models
+{
+    public class CommentModel
+    {
+        public string NickName { get; set; }
+        public string Email { get; set; }
+        public string Content { get; set; }
+    }
+}
