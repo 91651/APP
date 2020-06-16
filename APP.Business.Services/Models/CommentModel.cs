d3e50c20b3f7f3cf7efc170d75e4613f1ccbf21f
LZH <mail@521.org.cn>
2020-06-16 18:11:55 +0800

    function dev

--- APP.Business.Services/Models/CommentModel.cs
+++ APP.Business.Services/Models/CommentModel.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace APP.Business.Services.Models
 {
     public class CommentModel
     {
+        [Required]
         public string Author { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Content { get; set; }
         public List<CommentModel> Comments { get; set; }
     }
