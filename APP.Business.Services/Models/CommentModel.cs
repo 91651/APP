ce1b51367efa6451e1cd6b30863a9af0cc14b92c
LZH <mail@521.org.cn>
2020-07-08 23:00:55 +0800

    comment

--- APP.Business.Services/Models/CommentModel.cs
+++ APP.Business.Services/Models/CommentModel.cs
@@ -10,11 +10,11 @@ namespace APP.Business.Services.Models
         public string Id { get; set; }
         public string ParentId { get; set; }
         public string OwnerId { get; set; }
-        [Required]
+        [Required, DataType(DataType.Text)]
         public string Author { get; set; }
-        [Required]
+        [Required, DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-        [Required]
+        [Required, DataType(DataType.Text)]
         public string Content { get; set; }
         public DateTime Created { get; set; }
         public List<CommentModel> Comments { get; set; }
