4212a772e3efc3854a7a46c8ab43f1575e266db9
LZH <mail@521.org.cn>
2020-07-09 18:51:06 +0800

    captcha

--- APP.Business.Services/Models/CommentModel.cs
+++ APP.Business.Services/Models/CommentModel.cs
@@ -10,11 +10,14 @@ namespace APP.Business.Services.Models
         public string Id { get; set; }
         public string ParentId { get; set; }
         public string OwnerId { get; set; }
-        [Required, DataType(DataType.Text)]
+        [Required(ErrorMessage = "请输入一个心仪的昵称")]
+        [MinLength(3, ErrorMessage = "昵称至少3个字符")]
         public string Author { get; set; }
-        [Required, DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "请输入有效的邮箱地址")]
+        [EmailAddress]
         public string Email { get; set; }
-        [Required, DataType(DataType.Text)]
+        [Required(ErrorMessage = "请输入想留下的话语")]
+        [MinLength(5, ErrorMessage = "评论内容至少5个字符")]
         public string Content { get; set; }
         public DateTime Created { get; set; }
         public List<CommentModel> Comments { get; set; }
