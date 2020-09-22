e259df40695a3f13d34d7e59d9c02505065f98a6
LZH <mail@521.org.cn>
2020-09-22 18:45:34 +0800

    feat: add head

--- APP.UI.Admin/ClientApp/src/layout/main/Header/index.tsx
+++ APP.UI.Admin/ClientApp/src/layout/main/Header/index.tsx
@@ -1,5 +1,7 @@
 import {defineComponent, ref} from 'vue';
-import {Layout} from 'ant-design-vue';
+import {useRouter, useRoute} from 'vue-router';
+import {Layout, Avatar, Menu, Dropdown, Breadcrumb} from 'ant-design-vue'
+import {SettingOutlined, SearchOutlined,MenuFoldOutlined, MenuUnfoldOutlined} from '@ant-design/icons-vue';
 import components from "./components";
 import styles from './index.less';
 
@@ -10,55 +12,76 @@ export default defineComponent({
     components: {...components},
     props: {
     collapsed: {
-      type: Boolean,
-    }
-  },
-    setup() {
-        return () => (
-            <>
-                <ALayoutHeader class="layout-header">
+        type: Boolean,
+      }
+    },
+    methods: {
+      emitClick(event: Event) {
+        this.$emit('hello-vue', event)
+      }
+    },
+    render() {
+      const username = ref(localStorage.getItem('username') || '');
+      const router = useRouter();
+      const route = useRoute();
+      const signout = () => {
+        console.log(router, '退出登录')
+        router.replace({
+          name: 'login',
+          query: {
+            redirect: route.fullPath
+          }
+        })
+      }
+      return <>
+        <Layout.Header class="layout-header">
     <div class="left-options">
-      <span onClick={() => $emit('update:collapsed', !collapsed)} class="menu-fold">
+      <span onClick={() => this.$emit('update:collapsed', !this.collapsed)} class="menu-fold">
             <menu-unfold-outlined v-if="collapsed"/>
             <menu-fold-outlined v-else/>
     </span>
       <a-breadcrumb>
-        <a-breadcrumb-item v-for="routeItem in route.matched" :key="routeItem.name">
-          <a>{{ routeItem.meta.title }}</a>
-          <template v-slot:overlay>
-            <a-menu v-if="routeItem.children.length">
-              <template v-for="childItem in routeItem.children">
-                <a-menu-item v-if="!childItem.meta.hidden" :key="childItem.path">
-                  <router-link :to="routeItem.path == '/' ? childItem.path : (routeItem.path + '/' + childItem.path)">
-                    {{ childItem.meta.title }}
+      {route.matched.map((routeItem, i) => {
+          <a-breadcrumb-item key={routeItem.name}>
+          <a>{routeItem.meta.title}</a>
+          <template v-slot="overlay">
+            {routeItem.children.length &&
+                <a-menu v-if="routeItem.children.length">
+                {routeItem.children.map((childItem, i) => {
+                  childItem.meta?.hidden && <a-menu-item key={childItem.path}>
+                  <router-link to={routeItem.path == '/' ? childItem.path : (routeItem.path + '/' + childItem.path)}>
+                    { childItem.meta.title }
                   </router-link>
                 </a-menu-item>
-              </template>
-            </a-menu>
+                })}
+              </a-menu>
+            }
+            
           </template>
-        </a-breadcrumb-item>
+          </a-breadcrumb-item>
+        })}
+        
       </a-breadcrumb>
     </div>
     <div class="right-options">
-      <SettingOutlined/>
+    <SettingOutlined/>
       <SearchOutlined/>
       <Dropdown>
         <a-avatar>{{ username }}</a-avatar>
-        <template v-slot:overlay>
+        <template v-slot="overlay">
           <a-menu>
             <a-menu-item>
               <a href="javascript:;">个人中心</a>
             </a-menu-item>
             <a-menu-divider/>
             <a-menu-item>
-              <a @click.prevent="doLogout">退出登录</a>
+              <a onClick={signout}>退出登录</a>
             </a-menu-item>
           </a-menu>
         </template>
       </Dropdown>
     </div>
-  </ALayoutHeader>
-            </>
-        );
-    },
+  </Layout.Header>
+      </>
+    }
 });
